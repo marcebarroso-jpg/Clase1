2c25e46aed8b046bb08308f80392741a810a9d36
Marcelo Barroso <marcebarroso@gmail.com>
2026-08-18 22:10:01 -0300

    primer entrega

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//console.log(20);
+
+let nombre;
+let apellido;
+let edad;
+const constanteNumerica = 10
+let suma;
+
+nombre=prompt("ingresá tu nombre");
+apellido=prompt("ingresa tu apellido");
+edad=prompt("ingresa tu edad")
+suma = parseFloat(edad) + constanteNumerica
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log("Hola tu nombre es: " + nombre +" " + apellido + " y tu edad es: " + edad)
+console.log("y tu edad en 10 años será: " + suma);
